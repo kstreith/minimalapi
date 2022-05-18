5dc1f77a59811bf430c04d8dbd54dcbcab8e2aee
Streithorst <kip.streithorst@appliedis.com>
2022-05-18 17:37:05 -0400

    Add MinimalApi Sample Project

---
+++ MinimalApiSample/MinimalApiSample.Tests/TestApplication.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace MinimalApiSample.Tests
+{
+    public class TestApplication : WebApplicationFactory<Program>
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+
+            });
+        }
+    }
+}
---
+++ MinimalApiSample/MinimalApiSample.Tests/WeatherTests.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
+namespace MinimalApiSample.Tests
+{
+    public class WeatherTests
+    {
+        [Fact]
+        public async Task Get_Forecast_Returns_Values()
+        {
+            var application = new TestApplication();
+            var client = application.CreateClient();
+            var data = await client.GetFromJsonAsync<List<WeatherForecast>>("/weatherforecast");
+
+            data.Should().HaveCount(5);
+        }
+    }
+}
